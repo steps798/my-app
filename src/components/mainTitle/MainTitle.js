ba83a703435c41d6ff62286c4985687762d979cf
steps798 <stephanievirdayanti@gmail.com>
2019-08-25 01:01:45 +0700

    add carousel

--- src/components/mainTitle/MainTitle.js
+++ src/components/mainTitle/MainTitle.js
@@ -1,12 +1,64 @@
 import React from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-import bgImage from '../assets/backgroundImage.jpeg';
+import { Carousel } from 'react-bootstrap';
+import bgImage1 from '../assets/grand israel.jpg';
+import bgImage2 from '../assets/old-city-jerusalem.jpg';
+import bgImage3 from '../assets/jerusalem-old-city.jpg';
+import bgImage4 from '../assets/dead-sea-tiberias-sea-of-galilee.jpg';
+import bgImage5 from '../assets/Dome-of-Church-of-the-Holy-Sepulchre.jpg';
 import styles from './styles.css';
 
 function MainTitle(){
   return (
     <div>
-      <img src={bgImage} id="bgImage" alt="bgImage" />
+      {/* <img src={bgImage} id="bgImage" alt="bgImage" /> */}
+      <Carousel 
+        style={{maxWidth: '100%'}} 
+        controls={false}
+        fade={true}
+        >
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            style={{maxWidth: '100%', height: 'auto'}}
+            src={bgImage1}
+            alt="image1"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            style={{maxWidth: '100%', height: 'auto'}}
+            src={bgImage2}
+            alt="image2"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            style={{maxWidth: '100%', height: 'auto'}}
+            src={bgImage3}
+            alt="image3"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            style={{maxWidth: '100%', height: 'auto'}}
+            src={bgImage4}
+            alt="image4"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            style={{maxWidth: '100%', height: 'auto'}}
+            src={bgImage5}
+            alt="image5"
+          />
+        </Carousel.Item>
+      </Carousel>
     </div>
     )
   }
--- src/layout/Page.js
+++ src/layout/Page.js
@@ -12,31 +12,56 @@ const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
+  defaultGridItem: {
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3)
+  },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
     border: 0
   },
+  header: {
+    border: 0,
+    position: 'absolute', /* Sit on top of the page content */
+    // display: none; /* Hidden by default */
+    width: '100%', /* Full width (cover the whole page) */
+    height: '100%', /* Full height (cover the whole page) */
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 2 /* Specify a stack order in case you're using a different order for other elements */
+  },
+  imageSlider: {
+    border: 0,
+    margin: 0,
+    padding: 0,
+    width: '100%', /* Full width (cover the whole page) */
+    height: '100%', /* Full height (cover the whole page) */
+    backgroundColor: 'rgba(0,0,0,0.5)', /* Black background with opacity */
+  }
 }));
 
 function Page() {
   const classes = useStyles();
   return (
     <div className="Page">
-      <div className={styles.backgroundClip}>
-      </div>
+      {/* <div className={styles.backgroundClip}>
+      </div> */}
       <div className={classes.root}> 
       <Grid container spacing={3}>
         {/* header, consider use sticky effect */}
-        <Grid item xs={12}>
-          <div className={classes.paper}><Header/></div>
+        <Grid item xs={12} style={{margin: 0, padding: 0, position: 'relative'}}>
+          <div className={classes.header}><Header/></div>
         </Grid>
         {/* main title */}
-        <Grid item xs={12}>
-          <div><MainTitle/></div>
+        <Grid item xs={12} style={{margin: 0, padding: 0}}>
+          <div className={classes.imageSlider}><MainTitle/></div>
         </Grid>
         {/* component 1 */}
         <Grid item xs={12} sm={6}>
+          <Button>Click me</Button>
           <Paper className={classes.paper}>xs=12 sm=6</Paper>
         </Grid>
         {/* component 2 */}
