26e3144355b997abea85f4ca947a763c053577a5
steph-v <stephanie.virdayanti@mekari.com>
2020-03-08 23:29:55 +0700

    add lazy load handler

---
+++ src/lazyLoaderHandler.js
@@ -0,0 +1,18 @@
+// lazy load images if tag loading is not supported by the browser (Chrome 76)
+// reference: https://web.dev/native-lazy-loading/
+
+function handleLazyLoad() {
+  if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll('img[loading="lazy"]');
+    images.forEach(img => {
+      img.src = img.dataset.src;
+    });
+  } else {
+    // fetch polyfill/third-party library
+    // Dynamically import the LazySizes library
+    // const script = document.createElement('script');
+    // script.src =
+    // 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
+    // document.body.appendChild(script);
+  }
+}
